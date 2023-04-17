a3d635ae9c3904bffa9f5778a163a92dc860bfca
RealGavinPrice <116835834+RealGavinPrice@users.noreply.github.com>
2023-04-16 23:31:34 -0400

    wrote thought routes js

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,36 @@
+const router = require('express').Router();
+// Track these const names to thought-controller
+const {
+  getAllThoughts,
+  getThoughtById,
+  addThought,
+  updateThought,
+  removeThought,
+  addReaction,
+  removeReaction,
+} = require('../../controllers/thought-controller');
+
+//GET all thoughts and POST at api/thoughts
+router
+  .route('/')
+  .get(getAllThoughts)
+  .post(addThought);
+
+// GET one thought, UPDATE and DELETE routes api/:thoughtId
+router
+  .route('/:thoughtId')
+  .get(getThoughtById)
+  .put(updateThought)
+  .delete(removeThought);
+
+// Adding reaction
+router
+  .route('/:thoughtId/reactions')
+  .post(addReaction);
+
+//Deleting reaction
+router
+  .route('/:thoughtId/reactions/:reactionId')
+  .delete(removeReaction);
+
+module.exports = router;
