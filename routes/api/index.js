5275a67c05a32edf1d641ab8443d72eb3d85bf69
RealGavinPrice <116835834+RealGavinPrice@users.noreply.github.com>
2023-04-16 22:47:31 -0400

    wrote api index js

---
+++ routes/api/index.js
@@ -0,0 +1,8 @@
+const router = require('express').Router();
+const thoughts = require('./thoughtRoutes');
+const users = require('./userRoutes');
+
+router.use('/thoughts', thoughts);
+router.use('/users', users);
+
+module.exports = router;
