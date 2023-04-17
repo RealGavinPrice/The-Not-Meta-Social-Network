41e5366f343d6a388c696c678435e00b6d72e44e
RealGavinPrice <116835834+RealGavinPrice@users.noreply.github.com>
2023-04-16 23:07:48 -0400

    wrote user routes js

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,4 +1,38 @@
 const router = require('express').Router();
 
 // NEED TO ADD CONTROLLER CONSTS
+const {
+    getAllUsers,
+    getUserbyId,
+    createUser,
+    updateUser,
+    deleteUser,
+    addToFriendList,
+    removeFromFriendList
+} = require('../../controllers/userController');
 
+//GET all users and POST at api/users
+router
+    .route('/')
+    .get(getAllUsers)
+    .post(createUser);
+
+// GET one user, UPDATE and DELETE routes api/users/:id
+router
+    .route('/:id')
+    .get(getUserbyId)
+    .put(updateUser)
+    .delete(deleteUser);
+
+// Adding to friend list
+router
+    .route('/:userId/friends/:friendId')
+    .post(addToFriendList);
+
+// Removing from friend list
+router
+    .route('/:userId/friends/:friendId')
+    .delete(removeFromFriendList);
+
+
+module.exports = router;
